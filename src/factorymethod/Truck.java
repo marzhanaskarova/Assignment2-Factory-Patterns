29fdae0fc635eedb14caeaf476b7181daa3f5ef6
Marzhan Askarova <254121@astanait.edu.kz>
2026-09-20 15:53:51 +0500

    Add Truck and Ship transports

---
+++ src/factorymethod/Truck.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class Truck implements Transport {
+
+    @Override
+    public void deliver(String cargo, String destination) {
+        System.out.println("Truck delivers " + cargo + " to " + destination);
+    }
+}
